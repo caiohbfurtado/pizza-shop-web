e7600bdc01a6e845a417a30607a69c906ddac622
Caio Barutti <caiohbfurtado@gmail.com>
2024-12-16 19:21:59 -0300

    first commit

---
+++ src/components/order-status.tsx
@@ -0,0 +1,40 @@
+export type TypeOrderStatus =
+  | 'pending'
+  | 'canceled'
+  | 'processing'
+  | 'delivering'
+  | 'delivered'
+
+interface OrderStatusProps {
+  status: TypeOrderStatus
+}
+
+const orderStatusMap: Record<TypeOrderStatus, string> = {
+  pending: 'Pendente',
+  canceled: 'Cancelado',
+  processing: 'Em preparo',
+  delivering: 'Em entrega',
+  delivered: 'Entregue',
+}
+
+export function OrderStatus({ status }: OrderStatusProps) {
+  return (
+    <div className="flex items-center gap-2">
+      {status === 'pending' && (
+        <span className="h-2 w-2 rounded-full bg-slate-400" />
+      )}
+      {['processing', 'delivering'].includes(status) && (
+        <span className="h-2 w-2 rounded-full bg-amber-500" />
+      )}
+      {status === 'delivered' && (
+        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+      )}
+      {status === 'canceled' && (
+        <span className="h-2 w-2 rounded-full bg-rose-500" />
+      )}
+      <span className="font-medium text-muted-foreground">
+        {orderStatusMap[status]}
+      </span>
+    </div>
+  )
+}
